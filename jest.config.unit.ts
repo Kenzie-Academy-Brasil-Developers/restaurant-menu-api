03bc4e7c2588487227bec6910254a00b32b13a91
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2024-01-02 12:10:34 -0300

    jest setup files

--- jest.config.unit.ts
+++ jest.config.unit.ts
@@ -4,5 +4,6 @@ export default {
     preset: "ts-jest",
     testEnvironment: "node",
     testMatch: ["**/__tests__/units/**/*.[jt]s?(x)"],
+    setupFilesAfterEnv: ["./src/__tests__/__mocks__/prisma.ts", "./src/__tests__/utils/reflectMetadata.ts"]
  };
  
